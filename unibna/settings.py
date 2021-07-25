d2f36133e455786b7663627fe7124b5c0ef6c5fd
unibna <nguyenduylan711@gmail.com>
2021-07-26 05:08:34 +0700

    modify default db

--- unibna/settings.py
+++ unibna/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = "hehe"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://unibna.herokuapp.com/']
+ALLOWED_HOSTS = ['unibna.herokuapp.com']
 
 
 # Application definition
