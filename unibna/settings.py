180d02c294989492d919246da8d2fb3e30f6257f
unibna <nguyenduylan711@gmail.com>
2021-07-26 05:17:22 +0700

    Modify settings

--- unibna/settings.py
+++ unibna/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+import django_on_heroku
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -135,7 +137,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
@@ -151,7 +153,6 @@ EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 #     BASE_DIR / 'static',
 # ]
 
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage' # for heroku deploy
 
 MEDIA_ROOT = BASE_DIR / 'media'
@@ -163,3 +164,4 @@ AUTH_USER_MODEL = 'user.CustomUser'
 # For Crispy Forms
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
+django_on_heroku.settings(locals())
