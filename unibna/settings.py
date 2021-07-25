45b5fbc5050478ad8caea0a4a229a45c0c37b791
unibna <nguyenduylan711@gmail.com>
2021-07-26 05:29:09 +0700

    Modify settings

--- unibna/settings.py
+++ unibna/settings.py
@@ -153,8 +153,8 @@ EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 #     BASE_DIR / 'static',
 # ]
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
- # for heroku deploy
+# for heroku deploy
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL  = '/media/'
